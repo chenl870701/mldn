63bd906ff29a71e0eacf2c62c25e129d0f12cdeb
chenl <379149871@qq.com>
2017-06-30 09:56:54 +0800

    update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,6 @@
 public Class HelloJava{
 	public static void main(String [] args){
+		System.out.println("hello world");
 		System.out.println("hello Yootk!");	
 		System.out.println("www.Yootk.com");	
 	}	
