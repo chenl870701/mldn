ed809f92e324ae8c168d540c0cff97548c51047f
chenl <379149871@qq.com>
2017-06-30 11:02:59 +0800

    Delete HelloJava.java File

--- HelloJava.java
+++
@@ -1,7 +0,0 @@
-public Class HelloJava{
-	public static void main(String [] args){
-		System.out.println("hello world");
-		System.out.println("hello Yootk!");	
-		System.out.println("www.Yootk.com");	
-	}	
-}
